debd14ed7ffa7875b6ec09d2cbb3e76b5b689580
František Cepák <cepak.frantisek@seznam.cz>
2024-12-14 15:19:06 +0100

    first commit

---
+++ src/cart-sensors.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
