a88abb6bd9dd3ff26fcbe2afc70ed6e28d8ca99a
František Cepák <cepak.frantisek@seznam.cz>
2025-03-31 02:27:33 +0200

    dokončení GUI a vylepšení měřícího sketche

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,128 +1,375 @@
-from tkinter import Tk, Button, ttk, StringVar
+# Knihovny
+from tkinter import Tk, Button, Label, Entry, Text, ttk
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pandas as pd
+import math
 
-test_file = "DATA.csv"
-reader = pd.read_csv(test_file)
+# Konstanty
+TEST_FILE = "DATA.csv"
+WHEEL_RADIUS = 0.02  # poloměr kola [m]
+FIG_X = 6  # šířka grafu
+FIG_Y = 5  # výška grafu
+DPI = 100  # rozlišení grafu
+GRAVITY = 9.81  # gravitační zrychlení [m/s^2]
 
-X_time = reader["currentTime"].tolist()
-p = 0
-X_actualTime = []
-for _ in X_time:
-    X_actualTime.append(X_time[p] - X_time[0])
-    p += 1
+# Vynulování a převod času v milisekundách na sekundy
+def X_actual_time(rawT):
+    X_actualTime = []
+    startTime = rawT[0]
+    for p in range(len(rawT)):
+        X_actualTime.append((rawT[p] - startTime) / 1000)
+    return X_actualTime
 
-Y_totalAngle = reader["totalAngle"].tolist()
-Y_accelY = reader["Y_val"].tolist()
+# Převod akcelerace osy Y z G na m/s^2 (Akcelerometr)
+def accelY_in_ms2(yAccel):
+    accelY = []
+    for x in yAccel:
+        accelY.append(x * GRAVITY)
+    return accelY
 
-Y_accelZ = reader["Z_val"].tolist()
-p = 0
-Y_accelZ_normalized = []
-for _ in Y_accelZ:
-    Y_accelZ_normalized.append(Y_accelZ[p] - 1)
-    p += 1
+# Převod akcelerace osy Z z G na m/s^2 (Akcelerometr)
+def accelZ_in_ms2(zAccel):
+    accelZ = []
+    for x in zAccel:
+        accelZ.append(x * GRAVITY)
+    return accelZ
 
-p = 0
-totalAcceleration = []
-for _ in Y_accelY, Y_accelZ_normalized:
-    totalAcceleration.append(((Y_accelY[p] ** 2) + (Y_accelZ_normalized[p] ** 2)) ** 0.5)
-    p += 1
+# součet 2 složek zrychlení a vynulování počáteční hodnoty (Akcelerometr)
+def acceleration_from_acm(yAccel, zAccel):
+    accFromAcm = []
+    accY0 = yAccel[0]  # počáteční hodnota akcelerace osy Y
+    accZ0 = zAccel[0]  # počáteční hodnota akcelerace osy Z
+    for p in range(len(yAccel)):
+        if (
+            yAccel[p] - accY0 < 0
+        ):  # pokud je hodnota záporná, vozíček zpomaluje, takže zrychlení je záporné
+            accFromAcm.append(
+                -math.sqrt(((yAccel[p] - accY0) ** 2) + ((zAccel[p] - accZ0) ** 2))
+            )
+        else:  # pokud je hodnota kladná, vozíček zrychluje
+            accFromAcm.append(
+                math.sqrt(((yAccel[p] - accY0) ** 2) + ((zAccel[p] - accZ0) ** 2))
+            )
+    return accFromAcm
 
-""" print(reader) """
+# Převod akcelerace na rychlost (Akcelerometr)
+def velocity_from_acceleration(acceleration):
+    velFromAcm = []
+    previousTime = X_actualTime[0]
+    v0 = 0
+    for p in range(len(acceleration)):
+        sectionTime = X_actualTime[p] - previousTime  # časový interval mezi měřeními
+        velFromAcm.append(v0 + acceleration[p] * sectionTime)  # výpočet rychlosti
+        v0 = velFromAcm[p]  # uložení spočtené rychlosti jako v0 pro další výpočet
+        previousTime = X_actualTime[p]  # aktualizace předchozího času
+    return velFromAcm
 
-def plot_1():
-    fig = Figure(figsize=(6, 5), dpi=100)
+# Výpočet absolutní hodnoty úhlu, aby výsledná data nebyla záporná (Rot. enkodér)
+def total_angle_corr(angleTotal):
+    totalAngleCorr = []
+    for x in angleTotal:
+        totalAngleCorr.append(abs(x))
+    return totalAngleCorr
+
+# Výpočet úhlu v radiánech mezi jednotlivými měřeními (Rot. enkodér)
+def sec_angle_in_rad(angleTotal):
+    secAngleInRad = []
+    previousAngle = angleTotal[0]  # počáteční hodnota úhlu
+    for x in angleTotal:
+        sectionAngle = x - previousAngle  # úhel mezi jednotlivými měřeními
+        secAngleInRad.append(math.radians(sectionAngle))  # převod úhlu na radiány
+        previousAngle = x  # aktualizace předchozího úhlu
+    return secAngleInRad
+
+# Výpočet vzdálenosti na základě úhlu (Rot. enkodér)
+def distance_from_rot_enc(secAngle):
+    distanceFromRotEnc = []
+    for x in secAngle:
+        distanceFromRotEnc.append(WHEEL_RADIUS * x)  # vzálenost = poloměr kola * úhel
+    return distanceFromRotEnc
+
+# výpočet rychlosti na základě vzdálenosti (Rot. enkodér)
+def velocity_from_rot_enc(distance):
+    velFromRotEnc = []
+    previousTime = X_actualTime[0]  # počáteční čas
+    for p in range(len(distance)):
+        sectionTime = X_actualTime[p] - previousTime  # časový interval mezi měřeními
+        if sectionTime == 0:
+            velFromRotEnc.append(0)  # pokud je časový interval 0, rychlost je 0
+        else:
+            velFromRotEnc.append(distance[p] / sectionTime)  # výpočet rychlosti
+        previousTime = X_actualTime[p]  # aktualizace předchozího času
+    return velFromRotEnc
+
+# výpočet zrychlení na základě rychlosti (Rot. enkodér)
+def acceleration_from_rot_enc(velocity):
+    accFromRotEnc = []
+    previousTime = X_actualTime[0]
+    for p in range(len(velocity)):
+        sectionTime = X_actualTime[p] - previousTime
+        if sectionTime == 0:
+            accFromRotEnc.append(0)  # pokud je časový interval 0, zrychlení je 0
+        else:
+            accFromRotEnc.append(velocity[p] / sectionTime)  # výpočet zrychlení
+            previousTime = X_actualTime[p]
+    return accFromRotEnc
+
+# Kreslení grafu
+def plot(X_data, Y_data, plotLabel, plotTitle, X_label, Y_label, gridPos):
+    fig = Figure(figsize=(FIG_X, FIG_Y), dpi=DPI)
 
     plot1 = fig.add_subplot(111)
-    plot1.plot(X_actualTime, Y_totalAngle, label="totalAngle")
-    plot1.set_title("Graf závislosti úhlu na čase")
-    plot1.set_xlabel("Čas [ms]")
-    plot1.set_ylabel("Úhel [°]")
+    plot1.plot(X_data, Y_data, label=plotLabel)
+    plot1.set_title(plotTitle)
+    plot1.set_xlabel(X_label)
+    plot1.set_ylabel(Y_label, labelpad=0)
     plot1.legend()
 
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
+    canvas.get_tk_widget().grid(row=2, column=gridPos)
 
-    canvas.get_tk_widget().grid(row=1, column=0)
-
-"""     toolbar = NavigationToolbar2Tk(canvas, root)
-    toolbar.update()
-
-    canvas.get_tk_widget().pack(side="bottom", fill="both", expand=True) """
-
-def plot_2():
-    fig = Figure(figsize=(6, 5), dpi=100)
-
-    plot2 = fig.add_subplot(111)
-    plot2.plot(X_actualTime, Y_accelY, label="Y_val")
-    plot2.set_title("Graf závislosti accelY na čase")
-    plot2.set_xlabel("Čas [ms]")
-    plot2.set_ylabel("accelY")
-    plot2.legend()
-
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-
-    canvas.get_tk_widget().grid(row=1, column=1)
-
-def plot_3():
-    fig = Figure(figsize=(6, 5), dpi=100)
-
-    plot3 = fig.add_subplot(111)
-    plot3.plot(X_actualTime, Y_accelZ_normalized, label="Z_val")
-    plot3.set_title("Graf závislosti accelZ na čase")
-    plot3.set_xlabel("Čas [ms]")
-    plot3.set_ylabel("accelZ")
-    plot3.legend()
-
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-
-    canvas.get_tk_widget().grid(row=1, column=2)
-
-def plot_empty():
-    fig = Figure(figsize=(6, 5), dpi=100)
+# Vytvoření prázdného grafu
+def plot_empty(column):
+    fig = Figure(figsize=(FIG_X, FIG_Y), dpi=DPI)
     emptyPlot = fig.add_subplot(111)
     emptyPlot.plot([], [])
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
-    canvas.get_tk_widget().grid(row=1, column=0)
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-    canvas.get_tk_widget().grid(row=1, column=1)
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas.draw()
-    canvas.get_tk_widget().grid(row=1, column=2)
+    canvas.get_tk_widget().grid(row=2, column=column)
 
-def plot_all():
-    plot_1()
-    plot_2()
-    plot_3()
+# Výběr dat na základě comboboxu pro vytvoření grafu (pomocí slovníku)
+def plot_from_combobox(gridPos, comboBoxNumber):
+    plots = {
+        "Rot. enkodér: Graf závislosti úhlu na čase": (
+            X_actualTime,
+            totalAngleCorr,
+            "Celkový úhel",
+            "Graf závislosti úhlu na čase",
+            "Čas [s]",
+            "Úhel [°]",
+        ),
+        "Rot. enkodér: Graf závislosti uražené vzdálenosti na čase": (
+            X_actualTime,
+            distFromRotEnc,
+            "uražená vzdálenost",
+            "Graf závislosti uražené vzdálenosti (z enkodéru) na čase",
+            "Čas [s]",
+            "Vzdálenost [m]",
+        ),
+        "Rot. enkodér: Graf závislosti rychlosti na čase": (
+            X_actualTime,
+            velFromRotEnc,
+            "okamžitá rychlost",
+            "Graf závislosti rychlosti (z enkodéru) na čase",
+            "Čas [s]",
+            "Rychlost [m/s]",
+        ),
+        "Rot. enkodér: Graf závislosti zrychlení na čase": (
+            X_actualTime,
+            accFromRotEnc,
+            "okamžité zrychlení",
+            "Graf závislosti zrychlení (z enkodéru) na čase",
+            "Čas [s]",
+            "Zrychlení [m/s^2]",
+        ),
+        "Akcelerometr: Graf závislosti zrychlení (osa Y) na čase": (
+            X_actualTime,
+            accelY_in_G,
+            "okamžité zrychlení v ose Y",
+            "Graf závislosti zrychlení v ose Y na čase",
+            "Čas [s]",
+            "accelY",
+        ),
+        "Akcelerometr: Graf závislosti zrychlení (osa Z) na čase": (
+            X_actualTime,
+            accelZ_in_G,
+            "okamžité zrychlení v ose Z",
+            "Graf závislosti zrychlení v ose Z na čase",
+            "Čas [s]",
+            "accelZ",
+        ),
+        "Akcelerometr: Graf závislosti zrychlení na čase": (
+            X_actualTime,
+            accFromAcm,
+            "okamžité celkové zrychlení",
+            "Graf závislosti celkového zrychlení na čase",
+            "Čas [s]",
+            "Zrychlení [m/s^2]",
+        ),
+        "Akcelerometr: Graf závislosti rychlosti na čase": (
+            X_actualTime,
+            velFromAcm,
+            "okamžitá rychlost",
+            "Graf závislosti rychlosti na čase",
+            "Čas [s]",
+            "Rychlost [m/s]",
+        ),
+    }
 
-""" def plot_from_combobox():
-    if comboBox.get() == "data1":
-        plot_1()
-    elif comboBox.get() == "data2":
-        plot_2()
-    elif comboBox.get() == "data3":
-        plot_3() """
+    selected_plot = comboBoxNumber.get()  # získání názvu vybraného grafu
+    if selected_plot in plots:
+        X_data, Y_data, plotLabel, plotTitle, X_label, Y_label = plots[
+            selected_plot
+        ]  # získání dat pro vybraný graf
+        plot(
+            X_data, Y_data, plotLabel, plotTitle, X_label, Y_label, gridPos
+        )  # vykreslení grafu
 
+# Vypočítání výsledků na základě zadaných a naměřených hodnot
+def calculate_result(weight, tilt, time, accRot, velRot, accAcm, velAcm):
+    weight = float(weight) / 1000  # převod hmotnosti na kg
+    # teoretické výpočty
+    force1 = (
+        weight * GRAVITY * math.sin(math.radians(float(tilt)))
+    )  # výpočet síly souběžné s nakloněnou rovinou působící na vozíček
+    caclAccel = force1 / weight  # výpočet zrychlení vozíčku
+    caclVel = caclAccel * float(time)  # výpočet rychlosti
+    # výpočty s naměřenými hodnotami
+    avgAccRot = sum(accRot) / len(accRot)  # průměrné zrychlení z rot. enkodéru
+    avgVelRot = sum(velRot) / len(velRot)  # průměrná rychlost z rot. enkodéru
+    avgAccAcm = sum(accAcm) / len(accAcm)  # průměrné zrychlení z akcelerometru
+    avgVelAcm = sum(velAcm) / len(velAcm)  # průměrná rychlost z akcelerometru
+    # Přepne widget Text do normálního stavu, aby bylo možné vpisovat, poté ho znovu nastaví na "disabled"
+    text1["state"] = "normal"
+    text1.delete("1.0", "end")
+    text1.insert(
+        "1.0",
+        f"Vypočtená síla F1: {force1:.3f} N\nTeoretický výpočet:\nVypočtené zrychlení: {caclAccel:.2f} m/s^2\nVypočtená rychlost: {caclVel:.2f} m/s\n",
+    )
+    text1.insert(
+        "2.0",
+        f"Naměřené hodnoty:\nPrůměrné zrychlení (z rot. enkodéru): {avgAccRot:.2f} m/s^2\nPrůměrná rychlost (z rot. enkodéru): {avgVelRot:.2f} m/s\n",
+    )
+    text1.insert(
+        "3.0",
+        f"Průměrné zrychlení (z akcelerometru): {avgAccAcm:.2f} m/s^2\nPrůměrná rychlost (z akcelerometru): {avgVelAcm:.2f} m/s\n",
+    )
+    text1["state"] = "disabled"
 
+# Načtení dat z CSV souboru
+reader = pd.read_csv(TEST_FILE)
+rawTime = reader["currentTime"].tolist()
+totalAngle = reader["totalAngle"].tolist()
+accelY_in_G = reader["Y_val"].tolist()
+accelZ_in_G = reader["Z_val"].tolist()
+
+# Kontrola platnosti dat
+if len(rawTime) == 0 or len(rawTime) != (
+    len(totalAngle) or len(accelY_in_G) or len(accelZ_in_G)
+):
+    raise ValueError("Vstupní data jsou prázdná nebo neplatná.")
+
+# Výpočty hodnot
+X_actualTime = X_actual_time(rawTime)
+totalAngleCorr = total_angle_corr(totalAngle)
+secAngleInRad = sec_angle_in_rad(totalAngleCorr)
+distFromRotEnc = distance_from_rot_enc(secAngleInRad)
+velFromRotEnc = velocity_from_rot_enc(distFromRotEnc)
+accFromRotEnc = acceleration_from_rot_enc(velFromRotEnc)
+accelY = accelY_in_ms2(accelY_in_G)
+accelZ = accelZ_in_ms2(accelZ_in_G)
+accFromAcm = acceleration_from_acm(accelY, accelZ)
+velFromAcm = velocity_from_acceleration(accFromAcm)
+
+# Vytvoření GUI
 root = Tk()
 
-root.state("zoomed")
+root.state("zoomed")  # nastavení okna na celou obrazovku
 root.title("Měření fyzikálního experimentu")
 
-""" dataTypes = ["data1", "data2", "data3"]
-comboBox = ttk.Combobox(root, values=dataTypes)
-comboBox.grid(column=0, row=0)
-comboBox.set(dataTypes[0])
-comboBox['state'] = 'readonly' """
+# Seznam dat pro comboboxy
+dataTypes = [
+    "Rot. enkodér: Graf závislosti úhlu na čase",
+    "Rot. enkodér: Graf závislosti uražené vzdálenosti na čase",
+    "Rot. enkodér: Graf závislosti rychlosti na čase",
+    "Rot. enkodér: Graf závislosti zrychlení na čase",
+    "Akcelerometr: Graf závislosti zrychlení (osa Y) na čase",
+    "Akcelerometr: Graf závislosti zrychlení (osa Z) na čase",
+    "Akcelerometr: Graf závislosti zrychlení na čase",
+    "Akcelerometr: Graf závislosti rychlosti na čase",
+]
+# Vytvoření comboboxů pro výběr dat
+comboBox1 = ttk.Combobox(root, values=dataTypes, width=60)
+comboBox1.grid(row=0, column=0)
+comboBox1.set(dataTypes[0])
+comboBox1["state"] = "readonly"
+comboBox2 = ttk.Combobox(root, values=dataTypes, width=60)
+comboBox2.grid(row=0, column=1)
+comboBox2.set(dataTypes[6])
+comboBox2["state"] = "readonly"
 
-button1 = Button(root, text="importovat data", fg="green", command=plot_all)
-button1.grid(row=0, column=0)
+# Vytvoření tlačítek pro vykreslení grafů
+button1 = Button(
+    root,
+    text="importovat data",
+    fg="green",
+    command=lambda: plot_from_combobox(
+        0, comboBox1
+    ),  # zavolání funkce pomocí lambda výrazu, aby se funkce provedla po stisknutí tlačítka
+)
+button1.grid(row=1, column=0)
+button2 = Button(
+    root,
+    text="importovat data",
+    fg="green",
+    command=lambda: plot_from_combobox(1, comboBox2),
+)
+button2.grid(row=1, column=1)
 
-plot_empty()
+note1 = Label(root, text="Jako desetinnou čárku použijte tečku.")
+note1.grid(row=3, column=0, sticky="n")
+
+# Vytvoření vstupních polí pro zadání hodnot
+label1 = Label(root, text="hmotnost vozíčku (v gramech):")
+label1.grid(row=3, column=0, sticky="e")
+entry1 = Entry(root)
+entry1.grid(row=3, column=1, sticky="w")
+label2 = Label(root, text="náklon roviny (ve stupních):")
+label2.grid(row=4, column=0, sticky="e")
+entry2 = Entry(root)
+entry2.grid(row=4, column=1, sticky="w")
+label3 = Label(root, text="čas pohybu vozíčku (v sekundách):")
+label3.grid(row=5, column=0, sticky="e")
+entry3 = Entry(root)
+entry3.grid(row=5, column=1, sticky="w")
+
+# Vytvoření tlačítek pro výpočet výsledků a resetování vstupních polí
+button3 = Button(
+    root,
+    text="vypočítat",
+    fg="green",
+    command=lambda: calculate_result(
+        entry1.get(),
+        entry2.get(),
+        entry3.get(),
+        accFromRotEnc,
+        velFromRotEnc,
+        accFromAcm,
+        velFromAcm,
+    ),
+)
+button3.grid(row=6, column=1, sticky="nw")
+button4 = Button(
+    root,
+    text="resetovat",
+    fg="orange",
+    command=lambda: (
+        entry1.delete(0, "end"),
+        entry2.delete(0, "end"),
+        entry3.delete(0, "end"),
+    ),
+)
+button4.grid(row=6, column=1, sticky="n")
+
+# Vytvoření textového pole pro zobrazení výsledků
+text1 = Text(root, height=10, width=50)
+text1.grid(row=6, column=0, columnspan=2, sticky="w")
+text1["state"] = "disabled"
+
+# Vytvoření prázdných grafů
+plot_empty(column=0)
+plot_empty(column=1)
 
 root.mainloop()
--- src/cart-sensors.ino
+++ src/cart-sensors.ino
@@ -8,65 +8,83 @@
 #include <SD.h>
 
 // SD
-File dataFile;
-const int chipSelect = 10;
+File dataFile;             // Vytvoření objektu pro zápis do souboru
+const int chipSelect = 10; // Pin pro zahájení komunikace s SD čtěčkou
+const char *fileName = "data.csv";
 
-// AS5600
-const int AS5600 = 0x36;
+// AS5600 (magnetický enkodér)
+const int AS5600 = 0x36; // I2C adresa AS5600
+const float degConst = 0.087890625;
+/* 12-bitový výstup AS5600 => 4096 hodnot (2^12) na úplný kruh (360°)
+=> 360°/4096 = 0.087890625° */
 int magnetStatus = 0;
-int lowbyte;
-word highbyte;
+int lowByte;   // 8-bitový integer pro uložení výstupu z AS5600
+word highByte; // 16-bitový unsigned integer (word) pro uložení výstupu z AS5600
 int rawAngle;
 float degAngle;
-const float degConst = 0.087890625;
 float numberOfTurns = 0;
-float AngleCorrected = 0;
+float angleCorrected = 0;
 float startAngle = 0;
 float totalAngle = 0;
 float previousDegAngle = 0;
 
-// ADXL345
-const int ADXL345 = 0x53;
+// ADXL345 (akcelerometr)
+const int ADXL345 = 0x53; // I2C adresa ADXL345
 float X_val, Y_val, Z_val;
+
+const int switchPin = 5; // Pin vypínače
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
-  pinMode(5, INPUT);
+  /* Serial.begin(115200); */
+  pinMode(switchPin, INPUT); // Deklarace pinu vypínače pro zahájení měření
 
+  // Inicializace SD karty
   if (!SD.begin(chipSelect))
   {
-  Serial.println("CHYBA: Inicializace SD selhala!");
-  while (true);
+    while (true)
+    {
+      Serial.println("CHYBA: Inicializace SD selhala!");
+      delay(1000);
+    }
   }
-  Serial.println("SD inicializace úspěšná!");
-  SD.remove("data.csv");
+  Serial.println("SD inicializace úspěšná");
 
-  dataFile = SD.open("data.csv", FILE_WRITE);
-  if (dataFile) {
-  Serial.println("Soubor data.csv vytvořen.");
-  dataFile.println("currentTime,totalAngle,Y_val,Z_val");
-  dataFile.close();
-  } else {
-  Serial.println("Chyba: Nelze vytvořit data.csv");
+  SD.remove(fileName); // Odstranění starého souboru, pokud existuje
+
+  // Vytvoření nového souboru pro zápis dat
+  dataFile = SD.open(fileName, FILE_WRITE);
+  if (dataFile)
+  {
+    Serial.println("Soubor data.csv vytvořen");
+    dataFile.println("currentTime,totalAngle,Y_val,Z_val");
+    dataFile.close();
+  }
+  else
+  {
+    Serial.println("Chyba: Nelze vytvořit data.csv!");
   }
   delay(50);
 
+  // Zahájení I2C komunikace
   Wire.begin();
-  Wire.setClock(800000L);
+  Wire.setClock(800000L); // Nastavení rychlosti I2C na 800kHz
+
+  // Inicializace ADXL345
   Wire.beginTransmission(ADXL345);
-  Wire.write(0x2D);
-  Wire.write(8);
+  Wire.write(0x2D); // Zápis do registru POWER_CTL
+  Wire.write(8);    // Aktivace měření (nastaví bit D3 = 1)
   Wire.endTransmission();
   delay(10);
 
+  // Kalibrace osy Z akcelerometru
   Wire.beginTransmission(ADXL345);
-  Wire.write(0x20);
-  Wire.write(1);
+  Wire.write(0x20); // Zápis do registru OFSZ
+  Wire.write(1);    // Zápis kalibrační hodnoty
   Wire.endTransmission();
   delay(10);
 
+  // Inicializace AS5600
   checkMagnet();
   ReadRawAngle();
   startAngle = degAngle;
@@ -74,22 +92,30 @@ void setup()
 
 void loop()
 {
-  while (digitalRead(5) == HIGH) {
+  dataFile = SD.open(fileName, FILE_WRITE); // Otevření souboru pro zápis dat
+
+  // Smyčka měření
+  while (digitalRead(5) == HIGH)
+  {
+    // Získání dat z AS5600 a ADXL345
     ReadRawAngle();
     correctAngle();
     Angle_total();
     Accelerometer();
-    unsigned long currentTime = millis();
 
-    Serial.print(currentTime);
+    unsigned long currentTime = millis(); // Získání času od spuštění programu v milisekundách
+
+    // Debugging:
+    /* Serial.print(currentTime);
     Serial.print(",");
     Serial.print(totalAngle);
     Serial.print(",");
     Serial.print(Y_val);
     Serial.print(",");
     Serial.println(Z_val);
-    delay(5);
-    dataFile = SD.open("data.csv", FILE_WRITE);
+    delay(5); */
+
+    // Zápis dat do souboru
     if (dataFile)
     {
       dataFile.print(currentTime);
@@ -99,93 +125,104 @@ void loop()
       dataFile.print(Y_val);
       dataFile.print(",");
       dataFile.println(Z_val);
-      dataFile.close();
+      dataFile.flush();
     }
-    else {
-    Serial.println("Chyba: Nelze otevřít data.csv");
+    else
+    {
+      Serial.println("Chyba: Nelze otevřít data.csv!");
     }
   }
+  dataFile.close(); // Zavření souboru po ukončení měření
 }
 
 void Accelerometer()
 {
+  // Získání dat z akcelerometru
   Wire.beginTransmission(ADXL345);
-  Wire.write(0x32);
+  Wire.write(0x32); // Zápis do registru DATAX0 pro čtení
   Wire.endTransmission(false);
-  Wire.requestFrom(ADXL345, 6, true);
-  X_val = (Wire.read() | Wire.read() << 8);
-  Y_val = (Wire.read() | Wire.read() << 8);
+  Wire.requestFrom(ADXL345, 6, true);     // Požadavek na 6 registrů (2 pro každou osu)
+  X_val = Wire.read() | Wire.read() << 8; // Čtení dat osy X, která není dál potřeba (operace OR a posun o 8 bitů pro sloučení dvou bajtů)
+  Y_val = Wire.read() | Wire.read() << 8; // Obdobně pro osu Y (děleno 256 (2^8) pro převod na G jednotky)
   Y_val = Y_val / 256;
-  Z_val = (Wire.read() | Wire.read() << 8);
+  Z_val = Wire.read() | Wire.read() << 8; // Stejně i pro osu Z
   Z_val = Z_val / 256;
 }
 
 void ReadRawAngle()
 {
+  Wire.beginTransmission(AS5600); // Zahájení komunikace s AS5600
+  Wire.write(0x0D);               // Zápis příkazu pro čtení úhlu (nižší bajt)
+  Wire.endTransmission();         // Ukončení komunikace
+  Wire.requestFrom(AS5600, 1);    // Požadavek na 1 bajt dat
+
+  while (Wire.available() == 0); // Čekání na dostupnost I2C
+  lowByte = Wire.read();         // Zápis nižšího bajtu
+
   Wire.beginTransmission(AS5600);
-  Wire.write(0x0D);
+  Wire.write(0x0C); // Zápis příkazu pro čtení (vyšší bajt)
   Wire.endTransmission();
   Wire.requestFrom(AS5600, 1);
 
   while (Wire.available() == 0);
-  lowbyte = Wire.read();
+  highByte = Wire.read(); // Zápis vyššího bajtu
 
-  Wire.beginTransmission(AS5600);
-  Wire.write(0x0C);
-  Wire.endTransmission();
-  Wire.requestFrom(AS5600, 1);
+  highByte = highByte << 8;      // Posun vyššího bajtu o 8 bitů doleva
+  rawAngle = highByte | lowByte; // Sloučení obou bajtů do jednoho 16-bitového čísla (operace OR)
 
-  while (Wire.available() == 0);
-  highbyte = Wire.read();
-
-  highbyte = highbyte << 8;
-  rawAngle = highbyte | lowbyte;
-
-  degAngle = rawAngle * degConst;
+  degAngle = rawAngle * degConst; // Převod na úhel ve stupních
 }
 
 void correctAngle()
 {
-  AngleCorrected = degAngle - startAngle;
-  if (AngleCorrected < 0)
+  angleCorrected = degAngle - startAngle; // Vynulování úhlu podle počátečního úhlu
+  if (angleCorrected < 0)                 // Pokud je úhel záporný, přičteme 360, aby byl v rozsahu 0°-360°
   {
-    AngleCorrected = AngleCorrected + 360;
+    angleCorrected = angleCorrected + 360;
   }
 }
 
 void Angle_total()
 {
-  float RotationChange = AngleCorrected - previousDegAngle;
-  if (RotationChange > 180)
+  // Získání počtu otáček
+  float RotationChange = angleCorrected - previousDegAngle; // Rozdíl mezi aktuálním a předchozím úhlem
+  if (RotationChange > 180)                                 // Rozdíl větší než 180 => -1 otáčka
   {
     numberOfTurns--;
   }
-  else if (RotationChange < -180)
+  else if (RotationChange < -180) // Rozdíl menší než 180 => +1 otáčka
   {
     numberOfTurns++;
   }
 
-  totalAngle = (360 * numberOfTurns) + AngleCorrected;
-  previousDegAngle = AngleCorrected;
+  totalAngle = (360 * numberOfTurns) + angleCorrected; // Celkový úhel = počet otáček * 360 + aktuální úhel
+  previousDegAngle = angleCorrected;                   // Uložení aktuálního úhlu jako předchozího pro další výpočet
 }
 void checkMagnet()
 {
-  while ((magnetStatus & 32) != 32)
+  // Kontrola magnetu
+  while ((magnetStatus & 32) != 32) // Běží, dokud není detekován magnet
   {
     magnetStatus = 0;
 
     Wire.beginTransmission(AS5600);
-    Wire.write(0x0B);
+    Wire.write(0x0B); // Zápis příkazu pro čtení stavu magnetu
     Wire.endTransmission();
     Wire.requestFrom(AS5600, 1);
 
-    while (Wire.available() == 0);
-    magnetStatus = Wire.read();
+    while (Wire.available() == 0) // Čekání na dostupnost I2C
+    {
+      Serial.println("Chyba: Senzor AS5600 neodpovídá! Čekám na odpověď senzoru...");
+      delay(1000);
+    }
+    magnetStatus = Wire.read(); // Zápis stavu magnetu do proměnné
 
-    Serial.print("Magnet status: ");
+    /* Výpis stavu magnetu pro ladění
+    Magnet je moc blízko: 100111, moc daleko: 10111, ve správné vzdálenosti: 110111  */
+    Serial.print("Stav magnetu: ");
     Serial.println(magnetStatus, BIN);
   }
 
-  Serial.println("Magnet detected");
+  Serial.println("Magnet detekován!");
   delay(1000);
 }
